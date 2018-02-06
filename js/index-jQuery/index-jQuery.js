fa9919774b17ef046fd9a1392f944e4612a9bd69
Mix Jiang <mixitaliano@gmail.com>
2018-02-06 16:12:50 +0800

    file tree and links setup

--- js/index-jQuery/index-jQuery.js
+++ js/index-jQuery/index-jQuery.js
@@ -1,3 +1,26 @@
-$(function() {
+$(document).ready(function(){
+  $("ul").click(function() {
+    $(this).fadeToggle(function() {
+      alert("hide");
+    })
+  });
+
+var app1 = $("#nonexistent");
+
+var callback = function() {
+  console.log("done!");
+};
+
+if (app1.length){
+  app1.fadeIn(300, callback);
+} else {
+  callback();
+}
+
+$( ".homepageimg" ).click(function() {
+  $(this).hide(500).delay(1500).show(300);
+})
+
+
 
 });
